69ddc7b7a61da709bb20bbf22e638d7c54ca2bb1
--Explosion-- <explosion.scratch@gmail.com>
2021-08-07 18:57:47 -0500

    Redid it all 😐 (🥳?)

--- crypto_client.js
+++ crypto_client.js
@@ -1,69 +1,38 @@
-
-window.cryptWorker = new Worker('crypto.js');
-window.key = null;
-
-getWebWorkerResponse("generate-keys").then((_key) => {
-	window.key = _key;
-	ready();
+// Assuming https://raw.githubusercontent.com/juhoen/hybrid-crypto-js/master/web/hybrid-crypto.min.js is imported
+var crypt = new Crypt({
+    aesStandard: "AES-CTR",
+    rsaStandard: "RSAES-PKCS1-V1_5"
 })
 
-export function encryptFile(file, destination){
-	//Because of how this works files can be sent only to one person. We need to get their destination key from the server
-	return new Promise(async (resolve, reject) => {
-		var chunks = [];
-		const CHUNK_SIZE = 300;//300 chars per encryption.
-		const reader = new FileReader();
-		reader.addEventListener("load", ({result}) => {
-			let dataUrl = result;
-			let dataUrl_chunks = chunkString(result, CHUNK_SIZE);
-			//Encrypt each chunk
-			chunks = dataUrl_chunks.map((section) => encrypt(section, destination));
-			//Wait for all of it.
-			await Promise.all(chunks);
-			resolve(chunks);
-		});
-		/** https://stackoverflow.com/questions/7033639/split-large-string-in-n-size-chunks-in-javascript */
-		function chunkString(str, length) {
-		  return str.match(new RegExp('.{1,' + length + '}', 'g'));
+var keyPromise = rsa.generateKeyPairAsync();
+
+export async function key(){
+	return await keyPromise;
+}
+
+export function encrypt({file, to}){
+	return new Promise(resolve => {
+		var reader = new FileReader();
+		reader.onload = () => {
+			//In case I forget server side code for this is as such:
+			/*
+			socket.on("get public key", ({id}, callback) => {
+				if (!keys[id]) callback({error: true, message: "Key not found"});
+				callback({error: false, key: keys[id]});
+			})
+			*/
+			const publicKey = await new Promise(res => {
+				socket.emit("get public key", {id: to}, ({error, key, message}) => {
+					if (error) {
+						console.error(message);
+						return
+					} else {
+						res(key);
+					}
+				})
+			});
+			crypt.encrypt(publicKey, reader.result);
 		}
-		reader.readAsDataURL(file);
-	});
-	
-}
-
-export async function decryptFile(chunks){
-	//Decrypted should equal the original data url of the file sent, each peice being `CHUNK_SIZE` length containing a section of it.
-	var decrypted = await Promise.all(chunks.map(decrypt));
-	return decrypted.join("");//Join it back together.
-}
-
-export async function encrypt(message, destinationKey){
-	return await getWebWorkerResponse("encrypt", [message, destinationKey])
-}
-export async function decrypt(message){
-	return await getWebWorkerResponse("decrypt", [message])
-}
-export function getWebWorkerResponse (messageType, messagePayload) {
-  return new Promise((resolve, reject) => {
-    // Generate a random message id to identify the corresponding event callback
-    const messageId = Math.floor(Math.random() * 100000)
-
-    // Post the message to the webworker
-    window.cryptWorker.postMessage([messageType, messageId].concat(messagePayload))
-
-    // Create a handler for the webworker message event
-    const handler = function (e) {
-      // Only handle messages with the matching message id
-      if (e.data[0] === messageId) {
-        // Remove the event listener once the listener has been called.
-        e.currentTarget.removeEventListener(e.type, handler)
-
-        // Resolve the promise with the message payload.
-        resolve(e.data[1])
-      }
-    }
-
-    // Assign the handler to the webworker 'message' event.
-    window.cryptWorker.addEventListener('message', handler)
-  })
+		reader.readAsDataURL(file)
+	})
 }
