2a9c9e171ec42fd54a41e4d5d85654382d088f5b
ExplosionScratc <>
2021-08-09 22:33:36 +0000

    Rate limiting

--- index.js
+++ index.js
@@ -7,6 +7,16 @@ const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 
+// set up rate limiter: maximum of five requests per minute
+var RateLimit = require('express-rate-limit');
+var limiter = new RateLimit({
+  windowMs: 1*60*1000, // 1 minute
+  max: 100
+});
+
+// apply rate limiter to all requests
+app.use(limiter);
+
 var keys = {};
 
 const FILE_SIZE_LIMIT = 200; /*mb*/
