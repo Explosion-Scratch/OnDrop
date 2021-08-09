fd17c92c80ef04e99daefdca4510b3e7a310193f
ExplosionScratc <>
2021-08-09 22:57:13 +0000

    No longer serve root folder

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ if (fs.existsSync(`${__dirname}/uploads`)) {
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, "public")));
 
 io.on("connection", (socket) => {
   var joined = false;
